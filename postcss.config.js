88d7bfe06ad461924493e71b51df31230961ce47
Leo Huynh <huynhtuananh21895@gmail.com>
2020-12-07 16:39:40 +0700

    Update project config

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
+const tailwindcss = require('tailwindcss');
 module.exports = {
 	plugins: [
-		require('tailwindcss'),
+		tailwindcss('./tailwind.config.js'),
 		require('autoprefixer'),
-		require('cssnano')({
-			preset: 'default',
-	}),
 	]
 }
